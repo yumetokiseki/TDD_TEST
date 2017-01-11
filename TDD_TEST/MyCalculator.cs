07b276bf13c0efd07e09cd64521c89dab5451518
yumet <yumet@LAPTOP-V8EAP409>
2017-01-11 21:46:19 +0800

    Uploadv2

--- TDD_TEST/MyCalculator.cs
+++ TDD_TEST/MyCalculator.cs
@@ -8,23 +8,8 @@ namespace TDD_TEST
 {
     public class MyCalculator
     {
-        public List<int> costAdd()
+        public List<int> add(string property,int num, List<Product> items)
         {
-            var items = new List<Product>
-            {
-                new Product { Id=1, Cost=1,Revenue=11,SellPrice=21},
-                new Product { Id=2, Cost=2,Revenue=12,SellPrice=22},
-                new Product { Id=3, Cost=3,Revenue=13,SellPrice=23},
-                new Product { Id=4, Cost=4,Revenue=14,SellPrice=24},
-                new Product { Id=5, Cost=5,Revenue=15,SellPrice=25},
-                new Product { Id=6, Cost=6,Revenue=16,SellPrice=26},
-                new Product { Id=7, Cost=7,Revenue=17,SellPrice=27},
-                new Product { Id=8, Cost=8,Revenue=18,SellPrice=28},
-                new Product { Id=9, Cost=9,Revenue=19,SellPrice=29},
-                new Product { Id=10, Cost=10,Revenue=20,SellPrice=30},
-                new Product { Id=11, Cost=11,Revenue=21,SellPrice=31},
-            };
-
             List<int> output = new List<int> { };
 
             int sum = 0;
@@ -32,57 +17,16 @@ namespace TDD_TEST
 
             for (int i = 0; i < items.Count; i++)
             {
-                if (group == 2 || i == items.Count - 1)
+                if (group == num - 1 || i == items.Count - 1)
                 {
-                    sum = sum + items[i].Cost;
+                    sum = sum + Convert.ToInt32(items[i].GetType().GetProperty(property).GetValue(items[i], null));
                     output.Add(sum);
                     group = 0;
                     sum = 0;
                 }
                 else
                 {
-                    sum = sum + items[i].Cost;
-                    group++;
-                }
-            }
-
-            return output;
-        }
-
-        public List<int> revenueAdd()
-        {
-            var items = new List<Product>
-            {
-                new Product { Id=1, Cost=1,Revenue=11,SellPrice=21},
-                new Product { Id=2, Cost=2,Revenue=12,SellPrice=22},
-                new Product { Id=3, Cost=3,Revenue=13,SellPrice=23},
-                new Product { Id=4, Cost=4,Revenue=14,SellPrice=24},
-                new Product { Id=5, Cost=5,Revenue=15,SellPrice=25},
-                new Product { Id=6, Cost=6,Revenue=16,SellPrice=26},
-                new Product { Id=7, Cost=7,Revenue=17,SellPrice=27},
-                new Product { Id=8, Cost=8,Revenue=18,SellPrice=28},
-                new Product { Id=9, Cost=9,Revenue=19,SellPrice=29},
-                new Product { Id=10, Cost=10,Revenue=20,SellPrice=30},
-                new Product { Id=11, Cost=11,Revenue=21,SellPrice=31},
-            };
-
-            List<int> output = new List<int> { };
-
-            int sum = 0;
-            int group = 0;
-
-            for (int i = 0; i < items.Count; i++)
-            {
-                if (group == 3 || i == items.Count - 1)
-                {
-                    sum = sum + items[i].Revenue;
-                    output.Add(sum);
-                    group = 0;
-                    sum = 0;
-                }
-                else
-                {
-                    sum = sum + items[i].Revenue;
+                    sum = sum + Convert.ToInt32(items[i].GetType().GetProperty(property).GetValue(items[i], null));
                     group++;
                 }
             }
@@ -91,7 +35,8 @@ namespace TDD_TEST
         }
     }
 
-    internal class Product
+
+    public class Product
     {
         public int Id { get; set; }
 
--- TDD_TESTTests/MyCalculatorTests.cs
+++ TDD_TESTTests/MyCalculatorTests.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ExpectedObjects;
+using static TDD_TEST.MyCalculator;
 
 namespace TDD_TEST.Tests
 {
@@ -15,13 +16,29 @@ namespace TDD_TEST.Tests
         [TestMethod()]
         public void CostAddTest()
         {
+
+            var items = new List<Product>
+            {
+                new Product { Id=1, Cost=1,Revenue=11,SellPrice=21},
+                new Product { Id=2, Cost=2,Revenue=12,SellPrice=22},
+                new Product { Id=3, Cost=3,Revenue=13,SellPrice=23},
+                new Product { Id=4, Cost=4,Revenue=14,SellPrice=24},
+                new Product { Id=5, Cost=5,Revenue=15,SellPrice=25},
+                new Product { Id=6, Cost=6,Revenue=16,SellPrice=26},
+                new Product { Id=7, Cost=7,Revenue=17,SellPrice=27},
+                new Product { Id=8, Cost=8,Revenue=18,SellPrice=28},
+                new Product { Id=9, Cost=9,Revenue=19,SellPrice=29},
+                new Product { Id=10, Cost=10,Revenue=20,SellPrice=30},
+                new Product { Id=11, Cost=11,Revenue=21,SellPrice=31},
+            };
+
             //arrange
             var target = new MyCalculator();
             var expected = new List<int> {6,15,24,21};
             var actual = new List<int> { };
 
             //act
-            actual = target.costAdd();
+            actual = target.add("Cost",3,items);
 
             //assert
             expected.ToExpectedObject().ShouldEqual(actual);
@@ -30,13 +47,28 @@ namespace TDD_TEST.Tests
         [TestMethod()]
         public void RevenueAddTest()
         {
+            var items = new List<Product>
+            {
+                new Product { Id=1, Cost=1,Revenue=11,SellPrice=21},
+                new Product { Id=2, Cost=2,Revenue=12,SellPrice=22},
+                new Product { Id=3, Cost=3,Revenue=13,SellPrice=23},
+                new Product { Id=4, Cost=4,Revenue=14,SellPrice=24},
+                new Product { Id=5, Cost=5,Revenue=15,SellPrice=25},
+                new Product { Id=6, Cost=6,Revenue=16,SellPrice=26},
+                new Product { Id=7, Cost=7,Revenue=17,SellPrice=27},
+                new Product { Id=8, Cost=8,Revenue=18,SellPrice=28},
+                new Product { Id=9, Cost=9,Revenue=19,SellPrice=29},
+                new Product { Id=10, Cost=10,Revenue=20,SellPrice=30},
+                new Product { Id=11, Cost=11,Revenue=21,SellPrice=31},
+            };
+
             //arrange
             var target = new MyCalculator();
             var expected = new List<int> { 50, 66, 60 };
             var actual = new List<int> { };
 
             //act
-            actual = target.revenueAdd();
+            actual = target.add("Revenue", 4, items);
 
             //assert
             expected.ToExpectedObject().ShouldEqual(actual);
